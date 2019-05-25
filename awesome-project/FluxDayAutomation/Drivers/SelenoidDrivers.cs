c633041843cdedb25020fe3391f64dd6d6f07576
viktorderkach777 <viktorderkach777@yandex.ua>
2019-05-26 01:28:04 +0300

    Update browsers and URL

--- awesome-project/FluxDayAutomation/Drivers/SelenoidDrivers.cs
+++ awesome-project/FluxDayAutomation/Drivers/SelenoidDrivers.cs
@@ -9,14 +9,14 @@ namespace FluxDayAutomation.Drivers
     {
         public const string CHROME = "chrome";
         public const string FIREFOX = "firefox";
-        public const string CHROME_V1 = "70.0";
-        public const string CHROME_V2 = "71.0";
-        public const string FIREFOX_V1 = "62.0";
-        public const string FIREFOX_V2 = "63.0";
+        public const string CHROME_V1 = "73.0";
+        public const string CHROME_V2 = "74.0";
+        public const string FIREFOX_V1 = "66.0";
+        public const string FIREFOX_V2 = "67.0";
         private const bool ENABLE_VNC = true;
         private const int BROWSER_WIDTH = 1920;
         private const int BROWSER_HEIGHT = 1080;
-        private const string URI = "http://172.17.0.1:4444/wd/hub";
+        private const string URI = "http://35.232.250.225:4444/wd/hub";
 
         static public IWebDriver CreateDriver(string browser, string version)
         {
