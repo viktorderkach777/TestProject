a80100044cae4ccceb33886d02c5b0f4bf8df22f
viktorderkach777 <viktorderkach777@yandex.ua>
2019-06-08 23:34:27 +0300

    remove employee enters for test

--- awesome-project/FluxDayAutomation/UITests/LoginUITest.cs
+++ awesome-project/FluxDayAutomation/UITests/LoginUITest.cs
@@ -12,10 +12,10 @@ namespace FluxDayAutomation.UITests
     [TestFixture(MANAGER_EMAIL, MANAGER_PASSWORD, MANAGER_USER_NAME, SelenoidDrivers.FIREFOX, SelenoidDrivers.FIREFOX_V2)]
     [TestFixture(MANAGER_EMAIL, MANAGER_PASSWORD, MANAGER_USER_NAME, SelenoidDrivers.CHROME, SelenoidDrivers.CHROME_V1)]
     [TestFixture(MANAGER_EMAIL, MANAGER_PASSWORD, MANAGER_USER_NAME, SelenoidDrivers.CHROME, SelenoidDrivers.CHROME_V2)]
-    [TestFixture(EMPLOYEE_EMAIL, EMPLOYEE_PASSWORD, EMPLOYEE_USER_NAME, SelenoidDrivers.FIREFOX, SelenoidDrivers.FIREFOX_V1)]
-    [TestFixture(EMPLOYEE_EMAIL, EMPLOYEE_PASSWORD, EMPLOYEE_USER_NAME, SelenoidDrivers.FIREFOX, SelenoidDrivers.FIREFOX_V2)]
-    [TestFixture(EMPLOYEE_EMAIL, EMPLOYEE_PASSWORD, EMPLOYEE_USER_NAME, SelenoidDrivers.CHROME, SelenoidDrivers.CHROME_V1)]
-    [TestFixture(EMPLOYEE_EMAIL, EMPLOYEE_PASSWORD, EMPLOYEE_USER_NAME, SelenoidDrivers.CHROME, SelenoidDrivers.CHROME_V2)]
+   // [TestFixture(EMPLOYEE_EMAIL, EMPLOYEE_PASSWORD, EMPLOYEE_USER_NAME, SelenoidDrivers.FIREFOX, SelenoidDrivers.FIREFOX_V1)]
+    //[TestFixture(EMPLOYEE_EMAIL, EMPLOYEE_PASSWORD, EMPLOYEE_USER_NAME, SelenoidDrivers.FIREFOX, SelenoidDrivers.FIREFOX_V2)]
+    //[TestFixture(EMPLOYEE_EMAIL, EMPLOYEE_PASSWORD, EMPLOYEE_USER_NAME, SelenoidDrivers.CHROME, SelenoidDrivers.CHROME_V1)]
+    //[TestFixture(EMPLOYEE_EMAIL, EMPLOYEE_PASSWORD, EMPLOYEE_USER_NAME, SelenoidDrivers.CHROME, SelenoidDrivers.CHROME_V2)]
     public class LoginUITest
     {
         private IWebDriver driver = null;
